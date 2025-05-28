502b45b649231e179477bc8b271f8554727d1a88
Nicholas Amoo <nickewusi99@gmail.com>
2025-05-28 13:30:32 +0000

    Added pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+size = int(input('Enter the size of the pattern: '))
+
+i = 1
+j = i
+
+while i <= size:
+    for j in range(0, size):
+        print('*', end='')
+    print()
+    i += 1
+    
+    
