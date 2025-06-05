89ecdbe93c5e41f4b6c1d64f49709db633bfb10c
Nicholas Amoo <nickewusi99@gmail.com>
2025-06-05 15:12:11 +0000

    Added datetime app

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,11 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print(f'Current date and time: {current_date}')
+    
+    
+def calculate_future_date():
+    number_of_days = int(input('Enter the number of days to add to the current date: '))
+    future_date = datetime.today() + timedelta(days=number_of_days)
+    print(f'Future date: {future_date}')
