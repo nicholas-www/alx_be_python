a807cdcae5d4b08585365e66978fadf5ee488bec
Nicholas Amoo <nickewusi99@gmail.com>
2025-06-13 13:28:38 +0000

    Updated bank script

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,14 +4,13 @@ class BankAccount:
         
     def deposit(self, amount):
         self.account_balance += amount
-        print(f'Deposited: ${amount}')
+   
         
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
-            print(f'Withdrew: ${amount}')
             return True
-        print(f'Insufficient funds.')
+        
         return False
         
     def display_balance(self):
